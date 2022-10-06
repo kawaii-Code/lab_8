d47cb52e65a2eb5640b1cabe3892ffc2bc93803d
Nia <108984073+kawaii-Code@users.noreply.github.com>
2022-10-06 13:16:26 +0300

    Create DaysBetweenYears.pas
    Считаем дни между двумя годами

---
+++ DaysBetweenYears.pas
@@ -0,0 +1,12 @@
+﻿begin
+  
+  var (year1, year2) := ReadInteger2('Введите два года:');
+  
+  Assert(year1 <= year2);
+  
+  var sumDays := 0;
+  for var i := year1 to year2 do 
+    sumDays += (i mod 4 = 0) and (not (i mod 100 = 0) or (i mod 400 = 0)) ? 366 : 365;
+  Println($'Дней между вашими годами: {sumDays}'); 
+  
+end.
