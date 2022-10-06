d625be722570aa7725894d08753bfc3256b6ca6c
Nia <108984073+kawaii-Code@users.noreply.github.com>
2022-10-06 13:04:26 +0300

    Create NewYear.pas
    Программа определяет, какой из двух жней ближе к новому году

---
+++ NewYear.pas
@@ -0,0 +1,10 @@
+﻿begin
+  
+  var (day1, day2) := ReadInteger2();
+  
+  Assert(day1.InRange(1, 356) and day2.InRange(1, 356));
+  
+  Println(day2 > day1 ? 'Второй день ближе к новому году' :
+                        'Первый день ближе к новому году');
+  
+end.
